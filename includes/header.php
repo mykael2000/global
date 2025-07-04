8ec17869c09fe8c0d402718311c47e89919d472d
Michael <eramehmichael2000@gmail.com>
2025-07-04 19:11:28 +0100

    upload

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,10 @@ include "../connection.php";
 ob_start();
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
     header("Location: signin.php");
